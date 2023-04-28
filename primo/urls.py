7a12e99af7de4a09a7343a3cfe1b3f0ee45a0994
Eric Ford <ericford@mac.com>
2023-04-27 22:20:39 -0400

    First attempt at changes to allow django 4 to run.

--- primo/urls.py
+++ primo/urls.py
@@ -6,45 +6,33 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('admin/', admin.site.urls, name='admin'),
-    path('email/', views.email,  name="email"),
-    path('entity_relationship_diagram/', views.entity_relation_diagram, name="erd"),
-    path('login/', views.log_in, name="login"),
-    path('logout/', views.logout_view, name="logout"),
-
-    path('download_success/',
-        views.download_success,
-        name="download_success"),
-
-    path('export_scalar/',
-    # path('exportCsvFile/(?P<current_table>\w+)/(?P<preview_only>\w+)',
-        views.export_scalar,
-        name="export_scalar"),
-
-    path('parameter_selection/(<current_table>\w+)',
+    path("", views.IndexView.as_view(), name="index"),
+    path("admin/", admin.site.urls, name="admin"),
+    path("email/", views.email, name="email"),
+    path("entity_relationship_diagram/", views.entity_relation_diagram, name="erd"),
+    path("login/", views.log_in, name="login"),
+    path("accounts/login/", views.log_in, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("download_success/", views.download_success, name="download_success"),
+    path("export_scalar/", views.export_scalar, name="export_scalar"),
+    path("export_3d/which_3d_output_type>", views.export_3d, name="export_3d"),
+    # If none given, defaults to Morphologika.
+    path("export_3d/", views.export_3d, name="export_3d"),
+    path(
+        "parameter_selection/<current_table>",
         views.parameter_selection,
-        name="parameter_selection"),
-
-    path('query_3d/(<which_3d_output_type>\w+)/(<preview_only>\w+)',
-        views.query_3d,
-        name="query_3d"),
-
-    path('query_scalar/(<preview_only>\w+)',
-        views.query_scalar,
-        name="query_scalar"),
-
-    path('query_setup/(<scalar_or_3d>\w+)',
-        views.query_setup,
-        name='query_setup'),
-
-    path('query_setup/', views.query_setup, name="query_setup"),
-
-    path('query_start/', views.query_start, name="query_start"),
+        name="parameter_selection",
+    ),
+    path("query_3d/", views.query_3d, name="query_3d"),
+    path("query_scalar/", views.query_scalar, name="query_scalar"),
+    path("query_setup/<scalar_or_3d>", views.query_setup, name="query_setup"),
+    # If there's no GET, it defaults to scalar.
+    path("query_setup/", views.query_setup, name="query_setup"),
+    path("query_start/", views.query_start, name="query_start"),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
