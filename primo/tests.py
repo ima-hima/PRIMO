8792dc25de87d5106e06f6fe568d7a0ad9acfdc9
Eric Ford <ericford@mac.com>
2023-05-16 15:34:12 -0400

    Removed tests.py

--- primo/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-class ViewsTests(TestCase):
-    def test_detail_view_with_a_past_question(self) -> None:
-        """
-        The detail view of a question with a pub_date in the past should
-        display the question's text.
-        """
