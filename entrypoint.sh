867508d9f5874c2cb5e7ab9e5e1d9e9bb34cd065
Eric Ford <ima-hima@users.noreply.github.com>
2026-06-24 11:36:45 -0400

    For deployment (#85)
    * Updated gitignore to ignore sql dump files.
    
    * Switched to gunicorn. Updated whitenoise in middleware.
    
    * Updated to gunicorn. Now ready for deployment. Also caught an interface css bug.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-exec python manage.py runserver 0.0.0.0:8000
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
